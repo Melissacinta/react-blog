d8c2c516dd4cd1d75395873af427c2fb3755cfb4
TechieNg <melissacinta@gmail.com>
2021-10-13 20:42:47 +0100

    done creating models

--- api/index.js
+++ api/index.js
@@ -1,6 +1,19 @@
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
-app.listen('5000', ()=>{
-    console.log("backend is running");
-})
+dotenv.config();
+
+mongoose
+.connect(process.env.MONGO_URL)
+.then(console.log('connected to mongo'))
+.catch(err=>console.log(err));
+
+app.use("/lama", (req, res) => { 
+  console.log("hey this is lama url");
+});
+
+app.listen("5000", () => {
+  console.log("backend is running");
+});
